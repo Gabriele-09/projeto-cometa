e7472274d41f5b039d67de3cdd89b47c9c447a87
Gabriele Ribeiro <gabiribeiro.089@gmail.com>
2022-04-29 00:13:37 -0300

    feat:Descrição do sistema home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,10 @@
 <div style="padding-left:16px">
   <h2>Home</h2>
   <p>Bem vindo ao Via Cep</p>
+  <p> Aqui você pode fazer consulta de clientes e fornecedores cadastrados em nosso sistema</p>
+  <p> Também disponibilizamos uma consulta de CEP para facilitar a busca do seu endereço.</p>
+  <p> Clique nas abas de Clientes ou Fornecedores ao lado para começar a navegar.</p>
+  
 </div>
 
 </body>
